9197c2edb2793f0835f49cbf649d5d801d352548
Subx1s0o <nazarsabardin@gmail.com>
2024-08-08 16:17:31 +0300

    add auth & user services

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import * as process from 'process';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const port = process.env.PORT || 8080;
   await app.listen(port, '0.0.0.0');
 }
