11ba3cc0980743b09f3cab217a4afef8072d7db3
Subx1s0o <nazarsabardin@gmail.com>
2024-08-10 14:25:09 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import * as process from 'process';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5000',
+      'https://guess-the-word-gtw.web.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   const port = process.env.PORT || 8080;
   await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
